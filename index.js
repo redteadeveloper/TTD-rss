5f54bdc17b2c29c6d6f02880064c29a1e902ab45
calvinjung <myadiuio@gmail.com>
2021-11-08 12:12:53 +0900

    Update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
-const { Webhook } = require('discord-webhook-node');
+const { convert } = require('html-to-text');
+const { Webhook, MessageBuilder } = require('discord-webhook-node');
 const hook = new Webhook("https://discord.com/api/webhooks/905606601514287136/_mrIJfGJnv-vxwARCCz7g9wT47SaQpibvTHabHXPsAyhCMmp-jDLcIlHaDbVCbg3V70V");
 
 const IMAGE_URL = 'https://opentrackers.org/wp-content/uploads/2013/07/thetradersden_banner_9-21-2013.png';
@@ -37,8 +38,16 @@ async function getRecentItems() {
         let date = item.title.match(/(\d{4}|xxxx)\-((0[1-9]|1[012])|xx)\-((0[1-9]|[12][0-9]|3[01])|xx)/);
         let title = date ? item.title.split(date[0]) : [ item.title ]
 
+        // `**${title[0]}**${date ? `/ ${date[0]}` : ""} ${title[1] ? `/${title[1]}` : ""}\n> ${item.link}`
+
         try {
-            await hook.send(`**${title[0]}**${date ? `/ ${date[0]}` : ""} ${title[1] ? `/${title[1]}` : ""}\n> ${item.link}`);
+            const embed = new MessageBuilder()
+                .setTitle(`**${title[0]}**${date ? `/ ${date[0]}` : ""} ${title[1] ? `/${title[1]}` : ""}`)
+                .setURL(item.link)
+                .setDescription(convert(item.content.replace(/<fieldset class="fieldset">(.*?)<\/fieldset>/, ''), { wordwrap: 130 }))
+                .setColor('#77dd77')
+                .setFooter(`Published at ${item.pubDate}`, "https://i.ibb.co/pW53h3g/Picture1.png")
+            await hook.send(embed);
             console.log('=> Successfully sent webhook');
         } catch (e) {
             console.log(e.message);
